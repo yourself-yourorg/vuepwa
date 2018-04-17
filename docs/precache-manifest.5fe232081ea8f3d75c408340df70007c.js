3a7db7fd97a99447a9201424b9c73cb97b0a3e6c
Yourself YourOrg <yourself.yourorg@gmail.com>
2018-04-17 06:52:04 -0400

    Trial : see if vue-kindergarten-example PWA will work in mobile
    Signed-off-by: Yourself YourOrg <yourself.yourorg@gmail.com>

---
+++ docs/precache-manifest.5fe232081ea8f3d75c408340df70007c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3596a41e156a548c515",
+    "url": "/js/vendor.e3596a41.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "9d1ad84fa4ec01a7eada",
+    "url": "/js/app.9d1ad84f.js"
+  },
+  {
+    "revision": "a8ba60a8c09078ef9fc41cc6f231c548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d1ad84fa4ec01a7eada",
+    "url": "/css/app.a81de745.css"
+  }
+];
